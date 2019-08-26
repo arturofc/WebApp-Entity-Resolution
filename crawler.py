808b13bd6545581b3f251cbe4e43d95e4fbdb9f1
root <arturo.f.corro@gmail.com>
2019-08-26 04:25:03 +0000

    added the second layer of api calls to search by the retrieved unique id for each entity and retrieve the revelant info

--- crawler.py
+++ crawler.py
@@ -7,7 +7,7 @@ class SosWebAppCrawler(scrapy.Spider):
     name = 'crawler'
     allowed_domains = ['firststop.sos.nd.gov']
     entity_search_api_url = 'https://firststop.sos.nd.gov/api/Records/businesssearch'
-    entity_info_api = ''
+    entity_info_api_base = 'https://firststop.sos.nd.gov/api/FilingDetail/business'
     headers = {
         'Content-Type': 'application/json',
         'charset': 'UTF-8',
@@ -29,10 +29,35 @@ class SosWebAppCrawler(scrapy.Spider):
         )
 
     def search_entities(self, response):
-        form_data = {}
         entities = EntityItem()
         raw_data = json.loads(response.body_as_unicode())
 
         for key in raw_data['rows']:
-            entities['entity_id'] = key
-            yield entities
+            yield scrapy.Request(
+                method='GET',
+                url=f"{self.entity_info_api_base}/{key}/false",
+                callback=self.get_entity_info,
+                headers=self.headers,
+                meta={'entities': entities, 'entity_id': key}
+            )
+
+    def get_entity_info(self, response):
+        entities = response.meta['entities']
+        entities['entity_id'] = response.meta['entity_id']
+        entities['comm_registered_entity'] = None
+        entities['registered_agent'] = None
+        entities['owner'] = None
+
+        raw_data = json.loads(response.body_as_unicode())
+
+        for element in raw_data['DRAWER_DETAIL_LIST']:
+            label = element['LABEL']
+            print(label)
+            if label == 'Commercial Registered Agent':
+                entities['comm_registered_entity'] = element['VALUE']
+            if label == 'Registered Agent':
+                entities['registered_agent'] = element['VALUE']
+            if label == 'Owner Name':
+                entities['owner'] = element['VALUE']
+
+        yield entities
--- entity.py
+++ entity.py
@@ -5,4 +5,4 @@ class EntityItem(scrapy.Item):
     entity_id = scrapy.Field()
     comm_registered_entity = scrapy.Field()
     registered_agent = scrapy.Field()
-    owners = scrapy.Field()
+    owner = scrapy.Field()
